79a2e7b33faec857538f7c412ad179e6097ac091
V Charumathi <vcharumathi29@gmail.com>
2026-07-27 16:41:02 +0530

    Add ArrayList for multiple books

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Book> books = new ArrayList<>();
 
         Book book1 = new Book(
                 101,
                 "Java Programming",
                 "James Gosling"
         );
+        Book book2 = new Book(102,"Python Basics","Guido van rossum");
+        Book book3 = new Book(103,"SQL","John");
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        // Display all books
+        for(Book book:books){
+            book.displayBookDetails();
+            System.out.println();
+        }
 
         // Get book details using getters
         System.out.println("Book ID: " + book1.getBookId());
